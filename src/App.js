55cd4c35819d2171a9109da2f8a900ecc8ff1ff0
leticia-chijo <leticia.chijo@gmail.com>
2024-03-21 14:51:04 -0300

    Aula - useState com arrays e objetos

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
+import { useState } from "react";
 import logo from "./images/logo.png";
 
 export default function App() {
+    const [tarefas, setTarefas] = useState(["Jogar Lolzinho", "Assistir Narutinho"]);
+    const [terminadas, setTerminadas] = useState([]);
+
+    function adicionarTarefa() {
+        const novaTarefa = prompt("Digite sua tarefa");
+        const novasTarefas = [...tarefas, novaTarefa];
+        setTarefas(novasTarefas);
+    }
+
+    function terminarTarefa(tarefaTerminada) {
+        const novoArray = [...terminadas, tarefaTerminada];
+        setTerminadas(novoArray);
+    }
+
     return (
         <div className="conteudo">
             <img src={logo} alt="logo" />
 
             <div className="adicionar-tarefa">
                 <input type="text" placeholder="Digite a tarefa..." />
-                <button className="botao-adicionar">+</button>
+                <button onClick={adicionarTarefa} className="botao-adicionar">+</button>
             </div>
 
             <ul className="lista-tarefas">
-                <li className={`tarefa`}>
-                    <ion-icon name="ellipse-outline"></ion-icon>
-                    Jogar Lolzinho
-                </li>
-                <li className={`tarefa`}>
-                    <ion-icon name="ellipse-outline"></ion-icon>
-                    Assistir Narutinho
-                </li>
-                <li className={`tarefa`}>
-                    <ion-icon name="ellipse-outline"></ion-icon>
-                    Comprar cafezinho
-                </li>
+                {tarefas.map((tarefa, index) => (
+                    <li
+                        className={`tarefa ${terminadas.includes(tarefa) ? "finalizada" : ""} `}
+                        key={index}
+                        onClick={() => terminarTarefa(tarefa)}
+                    >
+                        <ion-icon name={terminadas.includes(tarefa) ? "checkmark-circle" : "ellipse-outline"}></ion-icon>
+                        {tarefa}
+                    </li>
+                ))}
             </ul>
         </div>
     )
