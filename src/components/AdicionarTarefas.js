e5b35db9fa452e02ada78326ccebd022a94b632f
leticia-chijo <leticia.chijo@gmail.com>
2024-03-21 17:22:41 -0300

    Aula - event e input controlado

--- src/components/AdicionarTarefas.js
+++ src/components/AdicionarTarefas.js
@@ -1,14 +1,22 @@
-export default function AdicionarTarefas({tarefas, setTarefas}) {
-    
+import { useState } from "react";
+
+export default function AdicionarTarefas({ tarefas, setTarefas }) {
+    const [textoTarefa, setTextoTarefa] = useState("");
+
     function adicionarTarefa() {
-        const novaTarefa = prompt("Digite sua tarefa");
-        const novasTarefas = [...tarefas, novaTarefa];
+        const novasTarefas = [...tarefas, textoTarefa];
         setTarefas(novasTarefas);
+        setTextoTarefa("");
     }
 
     return (
         <div className="adicionar-tarefa">
-            <input type="text" placeholder="Digite a tarefa..." />
+            <input
+                type="text"
+                placeholder="Digite a tarefa..."
+                onChange={(e) => setTextoTarefa(e.target.value)}
+                value={textoTarefa}
+            />
             <button onClick={adicionarTarefa} className="botao-adicionar">+</button>
         </div>
     );
